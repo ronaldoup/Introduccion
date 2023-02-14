c1ae6e1a8b625ba4eb18ce78add76cd6643f2280
ronaldoup <119707867+ronaldoup@users.noreply.github.com>
2023-02-14 08:27:21 -0500

    views

---
+++ core/views.py
@@ -0,0 +1,6 @@
+from django.views.generic import View
+
+class HomeView(View):
+    def get(self, request, *args, **kwargs):
+        context={  }
+        return render(request,'index.html',context)
